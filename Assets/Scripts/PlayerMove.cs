3c16247de348e9263b4b2f451d83977afc063c32
riccardo <r.iervolino93@gmail.com>
2019-10-30 14:05:33 +0100

    calibrating the jump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,8 +7,8 @@ public class PlayerMove : MonoBehaviour {
 	public float speed = 5;
 	public float jumpForce = 1;
 	[Space]
-	public float toShrinkWait = 0.33f;
-	public float stopShrinkWait = 0.66f;
+	public float toShrinkWait = 0.7f;
+	public float stopShrinkWait = 2f;
 	public float jumpingWait = 1f;
 
 	bool jumping, shrinking = false;
@@ -50,16 +50,16 @@ public class PlayerMove : MonoBehaviour {
 	}
 	IEnumerator toShrink() {
 
-		yield return new WaitForSeconds(toShrinkWait);
+		yield return new WaitForSeconds(toShrinkWait + shrinkage*0.02f);
 		shrinking = false;
 	}
 	IEnumerator stopShrink() {
 
-		yield return new WaitForSeconds(stopShrinkWait);
+		yield return new WaitForSeconds(stopShrinkWait + shrinkage * 0.02f);
 		if (!shrinking) {
 			jumping = true;
 			Debug.Log("ciao");
-			rigidbody.AddForce(Vector3.up * jumpForce * shrinkage, ForceMode.Impulse);
+			rigidbody.AddForce((Vector3.up * jumpForce * shrinkage)/3, ForceMode.Impulse);
 			shrinkage = 0;
 			StartCoroutine("Jumping");
 		}
@@ -70,13 +70,14 @@ public class PlayerMove : MonoBehaviour {
 		jumping = false;
 	}
 	void updateShrink() {
+        float _const = 3+shrinkage/3;
 
-		Up.MovePosition(Up.position + Vector3.down * shrinkage * 5 * Time.deltaTime);
-		Down.MovePosition(Down.position + Vector3.up * shrinkage * 5 * Time.deltaTime);
-		Left.MovePosition(Left.position + Vector3.left * shrinkage * 5 * Time.deltaTime);
-		Right.MovePosition(Right.position + Vector3.right * shrinkage * 5 * Time.deltaTime);
-		Front.MovePosition(Front.position + Vector3.forward * shrinkage * 5 * Time.deltaTime);
-		Back.MovePosition(Back.position + Vector3.back * shrinkage * 5 * Time.deltaTime);
+        Up.MovePosition(Up.position + Vector3.down * shrinkage * _const * Time.deltaTime);
+		Down.MovePosition(Down.position + Vector3.up * shrinkage * _const * Time.deltaTime);
+		Left.MovePosition(Left.position + Vector3.left * shrinkage * _const * Time.deltaTime);
+		Right.MovePosition(Right.position + Vector3.right * shrinkage * _const * Time.deltaTime);
+		Front.MovePosition(Front.position + Vector3.forward * shrinkage * _const * Time.deltaTime);
+		Back.MovePosition(Back.position + Vector3.back * shrinkage * _const * Time.deltaTime);
 		shrinking_counter--;
 
 	}
