fe074ec2f7e88aad58bd0be41b69a2f14bc25fc6
jebbo <r.iervolino93@gmail.com>
2019-11-06 23:07:52 +0100

    throwableObjects

--- Assets/Scripts/ThrowObjects.cs
+++ Assets/Scripts/ThrowObjects.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ThrowObjects : MonoBehaviour
 {
+    GameObject obj=null;
+    BoxCollider objBoxCollider;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,36 @@ public class ThrowObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(obj && Input.GetButtonDown("Jump"))
+        {
+            
+            StartCoroutine(Throw());
+
+        }
     }
-    
+    IEnumerator Throw()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        obj.GetComponent<ThrowableObject>().enabled = false;
+        if (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward != Vector3.zero)
+            obj.GetComponent<Rigidbody>().AddForce((Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward+Vector3.up/2) * 1000);
+        else
+            obj.GetComponent<Rigidbody>().AddForce(Vector3.up * 1000);
+        obj = null;
+
+        yield return new WaitForSeconds(0.2f);
+        
+        objBoxCollider.enabled = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 12&&obj==null)
+        {
+            other.gameObject.GetComponent<ThrowableObject>().enabled = true;
+            obj = other.gameObject;
+            objBoxCollider = obj.GetComponent<BoxCollider>();
+            objBoxCollider.enabled = false;
+        }
+    }
 }
---
+++ Assets/Scripts/ThrowableObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableObject : MonoBehaviour
+{
+    JellyCore core;
+    Rigidbody rigidbody;
+    BoxCollider collider;
+    Quaternion baseRotation;
+    FlapperState state;
+
+    SphereCollider coreCollider;
+    Vector3 localPos;
+
+    Vector3 lastGoodPosition;
+
+
+    void Awake()
+    {
+        core = FindObjectOfType<JellyCore>();
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
+        rigidbody.drag = JellyCore.drag;
+        baseRotation = rigidbody.rotation;
+        
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (state == FlapperState.solid)
+        {
+            
+                transform.position = core.transform.position + localPos;
+                transform.rotation = core.transform.rotation;
+            
+        }
+        else
+        {
+            if (state == FlapperState.gaseous)
+            {
+                rigidbody.AddForce(Physics.gravity * -JellyCore.gaseousAntiGravity);
+            }
+            Vector3 force = (core.transform.position - transform.position) * JellyCore.cohesion/2;
+            force.y = Mathf.Clamp(force.y, Physics.gravity.y, float.MaxValue);
+            rigidbody.AddForce(force);
+
+            rigidbody.MoveRotation(baseRotation);
+        }
+
+        //rigidbody.drag = JellyCore.drag;
+    }
+}
