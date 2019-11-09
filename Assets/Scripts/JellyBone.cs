505c389a64a51ed9a85b0213997b668dce344ce0
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-11-09 17:12:38 +0100

    Added pushable prefabs (light and heavy). Moved materials in Environments folder to Materials folder.

--- Assets/Scripts/JellyBone.cs
+++ Assets/Scripts/JellyBone.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class JellyBone : MonoBehaviour {
 	public bool isRoot;
+	public FlapperState state;
+
 	JellyCore core;
 	Rigidbody coreRigidbody;
 	Rigidbody rigidbody;
 	SphereCollider collider;
 	Quaternion baseRotation;
-	FlapperState state;
 
 	SphereCollider coreCollider;
 	Vector3 localPos;
@@ -100,16 +101,16 @@ public class JellyBone : MonoBehaviour {
 				|| Physics.Raycast(transform.position + Vector3.back, (core.transform.position - transform.position).normalized, out hit, 0.5f, layerMask)
 				|| Physics.Raycast(transform.position + Vector3.forward, (core.transform.position - transform.position).normalized, out hit, 0.5f, layerMask)
 				) {
-                Debug.DrawRay(transform.position, Vector3.up * 10, Color.green, 20, true);
-                
-                rigidbody.AddForce((Vector3.up*10), ForceMode.Acceleration);
-                //Debug.Log(lastGoodPosition);
-                //transform.LookAt(transform.position + lastGoodPosition);
-                //rigidbody.AddForce((transform.forward * 10), ForceMode.Acceleration);
-                //Debug.DrawRay(transform.position, (transform.forward * 2 + Vector3.up * 4) * 200f, Color.red, 20, true);
-                //continues in elseif
-            }
-        } /*else if (Vector3.Distance(core.transform.position, transform.position) < 0.75f) {
+				Debug.DrawRay(transform.position, Vector3.up * 10, Color.green, 20, true);
+
+				rigidbody.AddForce((Vector3.up * 10), ForceMode.Acceleration);
+				//Debug.Log(lastGoodPosition);
+				//transform.LookAt(transform.position + lastGoodPosition);
+				//rigidbody.AddForce((transform.forward * 10), ForceMode.Acceleration);
+				//Debug.DrawRay(transform.position, (transform.forward * 2 + Vector3.up * 4) * 200f, Color.red, 20, true);
+				//continues in elseif
+			}
+		} /*else if (Vector3.Distance(core.transform.position, transform.position) < 0.75f) {
 			lastGoodPosition = (core.transform.position - transform.position);
  		}*/
 
---
+++ Assets/Scripts/PushableHeavy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableHeavy : MonoBehaviour {
+	Rigidbody rigidbody;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		JellyBone jellyBone = other.GetComponent<JellyBone>();
+		if (jellyBone) {
+			if (jellyBone.state == FlapperState.solid) {
+				rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+			} else {
+				rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+			}
+		}
+	}
+}
