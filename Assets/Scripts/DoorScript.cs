5932cb91ee81e66fda8f6410cb64ca20050c244c
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2019-11-12 12:56:28 +0100

    Door Script Changed

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour, I_Activable
 {
     #region Attributes
-    public Vector3 upperLockerPosition;
-    public Vector3 lowerLockerPosition;
+    public float upperLockerY;
+    public float lowerLockerY;
     private bool isActive = false;
+    private Vector3 translateVector;
     #endregion
+
+    private void Start()
+    {
+        translateVector = new Vector3(0f, Time.fixedDeltaTime, 0f);
+    }
 
     private void FixedUpdate()
     {
-        if (isActive && Vector3.Distance(transform.position, upperLockerPosition) > Time.fixedDeltaTime)
-            transform.position = Vector3.MoveTowards(transform.position, upperLockerPosition, Time.fixedDeltaTime);
-        else if(!isActive && Vector3.Distance(transform.position, lowerLockerPosition) > Time.fixedDeltaTime)
-            transform.position = Vector3.MoveTowards(transform.position, lowerLockerPosition, Time.fixedDeltaTime);
+        if (isActive && (upperLockerY - transform.position.y) > Time.fixedDeltaTime)
+            transform.Translate(translateVector);
+        else if (!isActive && (transform.position.y - lowerLockerY) > Time.fixedDeltaTime)
+            transform.Translate(-translateVector);
+
+        isActive = false;
     }
 
     public void Activate()
