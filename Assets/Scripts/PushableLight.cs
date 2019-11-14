e47b4d689a88ad85b6d4ecaff4f81b6a49bc61d6
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-11-14 19:19:28 +0100

    Fixed pushable objects.

--- Assets/Scripts/PushableLight.cs
+++ Assets/Scripts/PushableLight.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PushableLight : MonoBehaviour {
+	[Tooltip("False = move on X")]
+	public bool moveOnZ = false;
+
 	Rigidbody rigidbody;
 
 	void Awake() {
-		rigidbody = GetComponent<Rigidbody>();
+		rigidbody = GetComponentInParent<Rigidbody>();
 	}
 
 	private void OnTriggerEnter(Collider other) {
 		JellyBone jellyBone = other.GetComponent<JellyBone>();
 		if (jellyBone) {
 			if (jellyBone.state != FlapperState.gaseous) {
-				rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+				if (moveOnZ) {
+					rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
+				} else {
+					rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+				}
 			} else {
 				rigidbody.constraints = RigidbodyConstraints.FreezeAll;
 			}
