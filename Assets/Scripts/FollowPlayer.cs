b04cd6029c9e7736946fff0eb15c7ab489362285
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-11-17 16:17:08 +0100

    Added stars in levels. Bugfix.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour {
+	[Header("To lock an axis, freeze Rigidbody position")]
 	public float minX = -100;
 	public float maxX = 100;
 	[Space]
-	public float minY = 0;
-	public float maxY = 0;
+	public float minY = -100;
+	public float maxY = 100;
 
 	JellyCore jellyCore;
 	Rigidbody rigidbody;
@@ -15,7 +16,7 @@ public class FollowPlayer : MonoBehaviour {
 	void Start() {
 		jellyCore = FindObjectOfType<JellyCore>();
 		rigidbody = GetComponent<Rigidbody>();
-		transform.position = new Vector3(Mathf.Clamp(jellyCore.transform.position.x, minX, maxX), Mathf.Clamp(jellyCore.transform.position.y, minY, maxY), transform.position.z);
+		rigidbody.MovePosition(new Vector3(Mathf.Clamp(jellyCore.transform.position.x, minX, maxX), Mathf.Clamp(jellyCore.transform.position.y, minY, maxY), transform.position.z));
 	}
 
 	void Update() {
