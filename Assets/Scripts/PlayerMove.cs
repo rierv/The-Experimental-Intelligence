8443bc62e05634bb970011a120dbf724ac1fee80
jebbo <r.iervolino93@gmail.com>
2019-11-17 18:02:02 +0100

    moving the core with lerp

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -55,7 +55,9 @@ public class PlayerMove : MonoBehaviour {
 
 	void FixedUpdate() {
 		if (canMove) {
-			rigidbody.MovePosition(rigidbody.position + (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward) * speed * Time.fixedDeltaTime);
+            transform.position = Vector3.Lerp(transform.position, transform.position + (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward), speed * Time.fixedDeltaTime);
+
+            //rigidbody.MovePosition(rigidbody.position + (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward) * speed * Time.fixedDeltaTime);
 		}
 	}
 
@@ -64,9 +66,13 @@ public class PlayerMove : MonoBehaviour {
 
 		if (stateManager.state == FlapperState.gaseous) {
 			if (Input.GetButton("Jump")) {
-				rigidbody.MovePosition(rigidbody.position + (Vector3.up * -gaseousShrinkDownForce * Time.deltaTime));
+                transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up, -gaseousShrinkDownForce * Time.deltaTime);
+
+                //rigidbody.MovePosition(rigidbody.position + (Vector3.up * -gaseousShrinkDownForce * Time.deltaTime));
 			} else {
-				rigidbody.MovePosition(rigidbody.position + (Vector3.up * gaseousFloatUpForce * Time.deltaTime));
+                transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up, gaseousFloatUpForce * Time.deltaTime);
+
+                rigidbody.MovePosition(rigidbody.position + (Vector3.up * gaseousFloatUpForce * Time.deltaTime));
 			}
 		} else {
 			if (Input.GetButtonDown("Jump") && !shrinking && !jumping && shrinkage < 2) {
