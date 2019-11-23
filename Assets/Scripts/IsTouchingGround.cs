e5792ea44f0e99e38d787829e3af71042fe1d44c
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-11-23 15:19:36 +0100

    Fixed IsTouchingGround.

--- Assets/Scripts/IsTouchingGround.cs
+++ Assets/Scripts/IsTouchingGround.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class IsTouchingGround : MonoBehaviour {
 	PlayerMove playerMove;
-    GameObject Flapper;
 	int colliderCount = 0;
+	float offset;
 
 	void Awake() {
-        playerMove = GetComponentInParent<PlayerMove>();
-        Flapper = GetComponentInParent<FlapperCore>().gameObject;
-    }
+		playerMove = GetComponentInParent<PlayerMove>();
+		offset = transform.localPosition.y;
+	}
 
-    private void Update()
-    {
-        transform.position = Vector3.Lerp(transform.position, Flapper.transform.localPosition + playerMove.transform.localPosition -Vector3.up, Time.deltaTime * 2);
-    }
-    private void OnTriggerEnter(Collider other) {
+	private void Update() {
+		transform.position = transform.parent.position + Vector3.up * offset;
+	}
+
+	private void OnTriggerEnter(Collider other) {
 		if (other.gameObject.layer != gameObject.layer) {
 			colliderCount++;
 			playerMove.canJump = colliderCount > 0;
