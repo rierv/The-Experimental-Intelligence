2e0ef3b2602ba941e8b9627ad1e89352032e30be
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-11-25 09:26:14 +0100

    Added Audio Manager prefab in all levels. Added Vertical Light prefab in most levels.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+	public static AudioManager singleton;
+	public AudioSource backgroundSource;
+	public AudioSource effectSource;
+	public AudioClip backgroundMusic;
+
+	private void Awake() {
+		if (singleton == null) {
+			singleton = this;
+		} else if (singleton != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start() {
+		PlayBackground(singleton.backgroundMusic);
+	}
+
+	public void PlayClip(AudioClip clip, bool loop = false) {
+		if (clip != null) {
+			singleton.effectSource.clip = clip;
+			singleton.effectSource.loop = loop;
+			singleton.effectSource.Play();
+		}
+	}
+
+	public void PlayBackground(AudioClip clip) {
+		if (clip != null && clip != singleton.backgroundSource.clip) {
+			singleton.backgroundSource.Stop();
+			singleton.backgroundSource.clip = clip;
+			singleton.backgroundSource.Play();
+		}
+	}
+}
