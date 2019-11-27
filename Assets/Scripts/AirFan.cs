de3b378e208a74818ec9ad3666f4ab1b5493b6d5
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-11-27 11:12:59 +0100

    Fixed exits destinations. Added basic air fan.

--- Assets/Scripts/AirFan.cs
+++ Assets/Scripts/AirFan.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Water : MonoBehaviour {
-	public float surface;
+public class AirFan : MonoBehaviour {
+	public Transform fan;
+	public float fanSpeed;
 	public float force = 4;
 	public float splashForce = 0.85f;
+	float surface;
+
+	void Awake() {
+		surface = transform.localScale.y;
+	}
+
+	void Update() {
+		fan.Rotate(transform.up, fanSpeed * Time.deltaTime);
+	}
 
 	private void OnTriggerEnter(Collider other) {
 		if (other.GetComponent<StateManager>() && other.GetComponent<StateManager>().state == FlapperState.jelly || other.GetComponent<JellyBone>() && other.GetComponent<JellyBone>().state == FlapperState.jelly) {
 			Rigidbody r = other.GetComponent<Rigidbody>();
 			r.useGravity = false;
-			r.AddForce(Vector3.up * -r.velocity.y * splashForce, ForceMode.VelocityChange);
+			r.AddForce(transform.up * -r.velocity.y * splashForce, ForceMode.VelocityChange);
 		}
 	}
 
 	private void OnTriggerStay(Collider other) {
 		if (other.GetComponent<StateManager>() && other.GetComponent<StateManager>().state == FlapperState.jelly || other.GetComponent<JellyBone>() && other.GetComponent<JellyBone>().state == FlapperState.jelly) {
-			other.GetComponent<Rigidbody>().AddForce(Vector3.up * (transform.position.y + surface - other.transform.position.y) * force, ForceMode.Acceleration);
+			other.GetComponent<Rigidbody>().AddForce(transform.up * (transform.position.y + surface - other.transform.position.y) * force, ForceMode.Acceleration);
 		}
 	}
 
