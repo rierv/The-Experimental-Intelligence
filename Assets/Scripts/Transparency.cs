216a83dccc52d779299abd49aff1b488e11f1feb
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-12-02 15:08:14 +0100

    Added gradual opacity for front wall.

---
+++ Assets/Scripts/Transparency.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour {
+	public float offset = 1;
+	public float decrease = 10;
+	float baseOpacity;
+	Material material;
+	Transform flapper;
+
+	void Awake() {
+		material = GetComponent<MeshRenderer>().material;
+		baseOpacity = material.GetColor("_BaseColor").a;
+		flapper = FindObjectOfType<JellyCore>().transform;
+	}
+
+	void Update() {
+		float opacity = Mathf.Clamp(-(flapper.position.z - transform.position.z) / decrease + offset, 0, 1) * baseOpacity;
+		Debug.Log(opacity);
+		material.SetColor("_BaseColor", new Color(1, 1, 1, opacity));
+	}
+}
