a1ae46acc70ebde5f09729bac3fae962efaf054b
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-12-06 09:40:26 +0100

    Some fixing for Start scene.

--- Assets/Scripts/HamsterCageScript.cs
+++ Assets/Scripts/HamsterCageScript.cs
@@ -2,83 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HamsterCageScript : MonoBehaviour
-{
-    #region Attributes
-    public GameObject targetObject;
-    Rigidbody wheel;
-    Transform[] dynamo;
-    GameObject[] electricity;
-    public float wheelSpeed = 200f;
-    private bool isActive = false;
-    private float horizontalInput;
-    public GameObject bobine;
-    #endregion
+public class HamsterCageScript : MonoBehaviour {
+	#region Attributes
+	public GameObject targetObject;
+	Rigidbody wheel;
+	Transform[] dynamo;
+	GameObject[] electricity;
+	public float wheelSpeed = 200f;
+	private bool isActive = false;
+	private float horizontalInput;
+	public GameObject bobine;
+	#endregion
 
-    private void Start()
-    {
-        wheel = gameObject.GetComponentInParent<Rigidbody>();
-        dynamo = bobine.GetComponentsInChildren<Transform>();
-        electricity = new GameObject[3];
-        electricity[0] = gameObject.transform.parent.parent.parent.Find("Battery").Find("BatteryElectricity").gameObject;
-        electricity[1] = gameObject.transform.parent.parent.parent.Find("Cable").Find("CableElectricity1").gameObject;
-        electricity[2] = gameObject.transform.parent.parent.parent.Find("Cable").Find("CableElectricity2").gameObject;
-        foreach (GameObject e in electricity)
-            e.SetActive(false);
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isActive = true;
-            other.GetComponent<Rigidbody>().drag = 4;
-        }
-    }
+	private void Start() {
+		wheel = gameObject.GetComponentInParent<Rigidbody>();
+		dynamo = bobine.GetComponentsInChildren<Transform>();
+		electricity = new GameObject[3];
+		electricity[0] = gameObject.transform.parent.parent.parent.Find("Battery").Find("BatteryElectricity").gameObject;
+		electricity[1] = gameObject.transform.parent.parent.parent.Find("Cable").Find("CableElectricity1").gameObject;
+		electricity[2] = gameObject.transform.parent.parent.parent.Find("Cable").Find("CableElectricity2").gameObject;
+		foreach (GameObject e in electricity)
+			e.SetActive(false);
+	}
+	private void OnTriggerStay(Collider other) {
+		if (other.CompareTag("Player")) {
+			isActive = true;
+			other.GetComponent<Rigidbody>().drag = 4;
+		}
+	}
 
-    private void OnTriggerExit(Collider other)
-    {
-        isActive = false;
-        other.GetComponent<Rigidbody>().drag = 1;
+	private void OnTriggerExit(Collider other) {
+		isActive = false;
+		other.GetComponent<Rigidbody>().drag = 1;
 
-        foreach (GameObject e in electricity)
-            e.SetActive(false);
-    }
+		foreach (GameObject e in electricity)
+			e.SetActive(false);
+	}
 
-    private void Update()
-    {
-        if (isActive)
-        {
-            horizontalInput = Input.GetAxis("Horizontal");
+	private void Update() {
+		if (isActive) {
+			horizontalInput = Input.GetAxis("Horizontal");
 
-            if (horizontalInput != 0)
-            {
-                wheel.transform.Rotate(new Vector3(0f, wheelSpeed * Time.deltaTime * -horizontalInput, 0f));
-                foreach (GameObject e in electricity)
-                    e.SetActive(true);
+			if (horizontalInput != 0) {
+				wheel.transform.Rotate(new Vector3(0f, wheelSpeed * Time.deltaTime * -horizontalInput, 0f));
+				foreach (GameObject e in electricity)
+					e.SetActive(true);
 
-                for (int i = 1; i < dynamo.Length; i++)
-                {
-                    dynamo[i].Rotate(new Vector3(0f, wheelSpeed * Time.deltaTime * -horizontalInput, 0f));
-                }
+				for (int i = 1; i < dynamo.Length; i++) {
+					dynamo[i].Rotate(new Vector3(0f, wheelSpeed * Time.deltaTime * -horizontalInput, 0f));
+				}
 
-                if (horizontalInput > 0)
-                {
-                    targetObject.GetComponent<I_Activable>().Deactivate();
-                    targetObject.GetComponent<I_Activable>().Activate(true);
-                }
-                else
-                {
-                    Debug.Log(horizontalInput);
-                    targetObject.GetComponent<I_Activable>().Deactivate();
-                    targetObject.GetComponent<I_Activable>().Activate(false);
-                }
-            }
-            else
-            {
-                foreach (GameObject e in electricity)
-                    e.SetActive(false);
-                targetObject.GetComponent<I_Activable>().Deactivate();
-            }
-        }
-    }
+				if (horizontalInput > 0) {
+					targetObject.GetComponent<I_Activable>().Deactivate();
+					targetObject.GetComponent<I_Activable>().Activate(true);
+				} else {
+					targetObject.GetComponent<I_Activable>().Deactivate();
+					targetObject.GetComponent<I_Activable>().Activate(false);
+				}
+			} else {
+				foreach (GameObject e in electricity)
+					e.SetActive(false);
+				targetObject.GetComponent<I_Activable>().Deactivate();
+			}
+		}
+	}
 }
--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -3,75 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LevelSelection : MonoBehaviour, I_Activable
-{
-    public NextLevel teleport;
-    public List<Object> levels;
-    bool active = false;
-    float counter = 0;
-    int currentLevel = 0;
-    
-    TMPro.TextMeshProUGUI levelName;
-    public void Activate(bool type = true)
-    {
-        active = true;
-    }
+public class LevelSelection : MonoBehaviour, I_Activable {
+	public NextLevel teleport;
+	public List<Object> levels;
+	bool active = false;
+	float counter = 0;
+	int currentLevel = 0;
 
-    public void canActivate(bool enabled)
-    {
-        throw new System.NotImplementedException();
-    }
+	TMPro.TextMeshProUGUI levelName;
+	public void Activate(bool type = true) {
+		active = true;
+	}
 
-    public void Deactivate()
-    {
-        active = false;
-    }
-    private void Awake()
-    {
-        levelName = GetComponent<TMPro.TextMeshProUGUI>();
-        setLevel();
-    }
+	public void canActivate(bool enabled) {
+		throw new System.NotImplementedException();
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public void Deactivate() {
+		active = false;
+	}
+	private void Awake() {
+		levelName = GetComponent<TMPro.TextMeshProUGUI>();
+		setLevel();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (active)
-        {
-            counter += Time.deltaTime * Input.GetAxis("Horizontal");
-            if (counter > 1 && currentLevel == levels.Count - 1) {
-                currentLevel = 0;
-                setLevel();
-            }
-            else if (counter < -1 && currentLevel == 0)
-            {
-                currentLevel = levels.Count - 1;
-                setLevel();
-            }
+	void Update() {
+		if (active) {
+			counter += Time.deltaTime * Input.GetAxis("Horizontal");
+			if (counter > 1 && currentLevel == levels.Count - 1) {
+				currentLevel = 0;
+				setLevel();
+			} else if (counter < -1 && currentLevel == 0) {
+				currentLevel = levels.Count - 1;
+				setLevel();
+			}
 
-            if (counter > 1) {
-                currentLevel++;
-                setLevel();
+			if (counter > 1) {
+				currentLevel++;
+				setLevel();
 
-            }
-            else if(counter< -1)
-            {
-                currentLevel--;
-                setLevel();
-            }
-        }
-    }
-    void setLevel()
-    {
-        teleport.nextLevel = levels[currentLevel];
-        
+			} else if (counter < -1) {
+				currentLevel--;
+				setLevel();
+			}
+		}
+	}
 
-        levelName.text = "Play the level " + (currentLevel+1) + " :\n" + teleport.nextLevel.name;
-        counter = 0;
-    }
+	void setLevel() {
+		teleport.nextLevel = levels[currentLevel];
+		levelName.text = "Play level " + (currentLevel + 1) + ":\n" + teleport.nextLevel.name + "?";
+		counter = 0;
+	}
 }
