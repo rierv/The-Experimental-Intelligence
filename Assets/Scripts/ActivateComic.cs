cf4df1bc63ee5e1085dce1c797f145d60c7fba2c
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-12-07 16:18:52 +0100

    Added more sounds.

--- Assets/Scripts/ActivateComic.cs
+++ Assets/Scripts/ActivateComic.cs
@@ -3,182 +3,159 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ActivateComic : MonoBehaviour, I_Activable
-{
-    #region Attributes
-    public bool comicOrientation;
-    public float duration;
-    public float fadeTime;
-    public float charWaitTime;
-    GameObject comicCloud;
-    private Color color;
-    private bool isActive = false;
-    private bool activateAnimation = false;
-    private bool disactivateAnimation = false;
-    public Vector3 standardScale;
-    private Vector3 littleScale;
-    private Vector3 bigScale;
-    private int animationPhase = 0;
-    GameObject textCloud;
-    public string[] completeTextPieces;
-    private string completeText = "";
-    private string text;
-    private Color textColor;
-    public bool active = true;
-    private float stretchLerp = 0f;
-    #endregion
+public class ActivateComic : MonoBehaviour, I_Activable {
+	#region Attributes
+	public bool comicOrientation;
+	public float duration;
+	public float fadeTime;
+	public float charWaitTime;
+	GameObject comicCloud;
+	private Color color;
+	private bool isActive = false;
+	private bool activateAnimation = false;
+	private bool disactivateAnimation = false;
+	public Vector3 standardScale;
+	private Vector3 littleScale;
+	private Vector3 bigScale;
+	private int animationPhase = 0;
+	GameObject textCloud;
+	public string[] completeTextPieces;
+	private string completeText = "";
+	private string text;
+	private Color textColor;
+	public bool active = true;
+	private float stretchLerp = 0f;
+	#endregion
 
-    private void Start()
-    {
-        comicCloud = gameObject.transform.Find("ComicCloud").gameObject;
-        comicCloud.transform.localScale = standardScale;
-        color = comicCloud.GetComponent<SpriteRenderer>().color;
-        color.a = 0f;
-        comicCloud.SetActive(false);
-        textCloud = comicCloud.transform.GetChild(0).gameObject;
-        textColor = textCloud.GetComponent<Text>().color;
-        bigScale = 1.1f * standardScale;
-        littleScale = 0.9f * standardScale;
-        text = "";
-        for (int k = 0; k < completeTextPieces.Length - 1; k++)
-        {
-            completeTextPieces[k] = completeTextPieces[k].ToUpper();
-            completeText += completeTextPieces[k] + "\n";
-        }
-        completeTextPieces[completeTextPieces.Length - 1] = completeTextPieces[completeTextPieces.Length - 1].ToUpper();
-        completeText += completeTextPieces[completeTextPieces.Length - 1];
-        if (comicOrientation)
-            comicCloud.GetComponent<SpriteRenderer>().flipX = true;
-    }
+	private void Start() {
+		comicCloud = gameObject.transform.Find("ComicCloud").gameObject;
+		comicCloud.transform.localScale = standardScale;
+		color = comicCloud.GetComponent<SpriteRenderer>().color;
+		color.a = 0f;
+		comicCloud.SetActive(false);
+		textCloud = comicCloud.transform.GetChild(0).gameObject;
+		textColor = textCloud.GetComponent<Text>().color;
+		bigScale = 1.1f * standardScale;
+		littleScale = 0.9f * standardScale;
+		text = "";
+		for (int k = 0; k < completeTextPieces.Length - 1; k++) {
+			completeTextPieces[k] = completeTextPieces[k].ToUpper();
+			completeText += completeTextPieces[k] + "\n";
+		}
+		completeTextPieces[completeTextPieces.Length - 1] = completeTextPieces[completeTextPieces.Length - 1].ToUpper();
+		completeText += completeTextPieces[completeTextPieces.Length - 1];
+		if (comicOrientation)
+			comicCloud.GetComponent<SpriteRenderer>().flipX = true;
+	}
 
-    private void Update()
-    {
-        if (activateAnimation)
-        {
-            StartCoroutine(StartAnimation());
-            StartCoroutine(AnimateText());
-            activateAnimation = false;
-        }
-        else if (disactivateAnimation)
-        {
-            StopAllCoroutines();
-            StartCoroutine(EndAnimation());
-            disactivateAnimation = false;
-        }
-    }
+	private void Update() {
+		if (activateAnimation) {
+			GetComponent<AudioSource>().Play();
+			StartCoroutine(StartAnimation());
+			StartCoroutine(AnimateText());
+			activateAnimation = false;
+		} else if (disactivateAnimation) {
+			StopAllCoroutines();
+			StartCoroutine(EndAnimation());
+			disactivateAnimation = false;
+		}
+	}
 
-    IEnumerator StartAnimation()
-    {        
-        Appear();
-        Stretch();
-        yield return new WaitForSeconds(Time.deltaTime / fadeTime);
-        yield return StartAnimation();
-    }
+	IEnumerator StartAnimation() {
+		Appear();
+		Stretch();
+		yield return new WaitForSeconds(Time.deltaTime / fadeTime);
+		yield return StartAnimation();
+	}
 
-    private void Appear()
-    {
-        color.a += Time.deltaTime / fadeTime;
-        color.a = Mathf.Clamp(color.a, 0, 1);
-        textColor.a = color.a;
-        comicCloud.GetComponent<SpriteRenderer>().color = color;
-        textCloud.GetComponent<Text>().color = textColor;
-    }
+	private void Appear() {
+		color.a += Time.deltaTime / fadeTime;
+		color.a = Mathf.Clamp(color.a, 0, 1);
+		textColor.a = color.a;
+		comicCloud.GetComponent<SpriteRenderer>().color = color;
+		textCloud.GetComponent<Text>().color = textColor;
+	}
 
-    private void Stretch()
-    {
-        switch (animationPhase)
-        {
-            case 0:
-                comicCloud.transform.localScale = Vector3.Lerp(standardScale, bigScale, stretchLerp);
-                break;
-            case 1:
-                comicCloud.transform.localScale = Vector3.Lerp(bigScale, littleScale, stretchLerp);
-                break;
-            case 2:
-                comicCloud.transform.localScale = Vector3.Lerp(littleScale, standardScale, stretchLerp);
-                break;
-            default:
-                break;
-        }
-        stretchLerp += (Time.deltaTime / fadeTime) * 3;
-        if (stretchLerp > 1)
-        {
-            animationPhase++;
-            stretchLerp = 0f;
-        }
-    }
+	private void Stretch() {
+		switch (animationPhase) {
+			case 0:
+				comicCloud.transform.localScale = Vector3.Lerp(standardScale, bigScale, stretchLerp);
+				break;
+			case 1:
+				comicCloud.transform.localScale = Vector3.Lerp(bigScale, littleScale, stretchLerp);
+				break;
+			case 2:
+				comicCloud.transform.localScale = Vector3.Lerp(littleScale, standardScale, stretchLerp);
+				break;
+			default:
+				break;
+		}
+		stretchLerp += (Time.deltaTime / fadeTime) * 3;
+		if (stretchLerp > 1) {
+			animationPhase++;
+			stretchLerp = 0f;
+		}
+	}
 
 
-    IEnumerator AnimateText()
-    {
-        int index;
-        int newLineCount;
-        for (index = 0, newLineCount = 1 ; index < completeText.Length; index++)
-        {
-            text += completeText[index];
+	IEnumerator AnimateText() {
+		int index;
+		int newLineCount;
+		for (index = 0, newLineCount = 1; index < completeText.Length; index++) {
+			text += completeText[index];
 
-            if (completeText[index] == '\n') 
-            {
-                newLineCount++;
-            }
-            if (newLineCount > 3)
-            {
-                text = text.Remove(0, text.IndexOf('\n'));
-                newLineCount--;
-            }
-            textCloud.GetComponent<Text>().text = text;
-            yield return new WaitForSeconds(charWaitTime);
+			if (completeText[index] == '\n') {
+				newLineCount++;
+			}
+			if (newLineCount > 3) {
+				text = text.Remove(0, text.IndexOf('\n'));
+				newLineCount--;
+			}
+			textCloud.GetComponent<Text>().text = text;
+			yield return new WaitForSeconds(charWaitTime);
 
-        }
-        if (index == completeText.Length)
-        {
-            yield return new WaitForSeconds(duration);
-            disactivateAnimation = true;
-        }
-    }
+		}
+		if (index == completeText.Length) {
+			yield return new WaitForSeconds(duration);
+			disactivateAnimation = true;
+		}
+	}
 
-    IEnumerator EndAnimation()
-    {
-        Disappear();
-        yield return new WaitForSeconds(Time.deltaTime / fadeTime);
-        yield return EndAnimation();
-    }
+	IEnumerator EndAnimation() {
+		Disappear();
+		yield return new WaitForSeconds(Time.deltaTime / fadeTime);
+		yield return EndAnimation();
+	}
 
-    private void Disappear()
-    {
-        color.a -= Time.deltaTime / fadeTime;
-        color.a = Mathf.Clamp(color.a, 0, 1);
-        textColor.a = color.a;
-        comicCloud.GetComponent<SpriteRenderer>().color = color;
-        textCloud.GetComponent<Text>().color = textColor;
-        if(color.a <= 0)
-        {
-            activateAnimation = false;
-            disactivateAnimation = false;
-            StopAllCoroutines();
-        }
-    }
+	private void Disappear() {
+		color.a -= Time.deltaTime / fadeTime;
+		color.a = Mathf.Clamp(color.a, 0, 1);
+		textColor.a = color.a;
+		comicCloud.GetComponent<SpriteRenderer>().color = color;
+		textCloud.GetComponent<Text>().color = textColor;
+		if (color.a <= 0) {
+			activateAnimation = false;
+			disactivateAnimation = false;
+			StopAllCoroutines();
+		}
+	}
 
-    public void Activate(bool type = true)
-    {
-        if (!isActive)
-        {
-            comicCloud.SetActive(true);
-            comicCloud.GetComponent<SpriteRenderer>().color = color;
-            isActive = true;
-            activateAnimation = true;
-            animationPhase = 0;
-        }
-    }
+	public void Activate(bool type = true) {
+		if (!isActive) {
+			comicCloud.SetActive(true);
+			comicCloud.GetComponent<SpriteRenderer>().color = color;
+			isActive = true;
+			activateAnimation = true;
+			animationPhase = 0;
+		}
+	}
 
-    public void canActivate(bool enabled)
-    {
-        throw new System.NotImplementedException();
-    }
+	public void canActivate(bool enabled) {
+		throw new System.NotImplementedException();
+	}
 
 
-    public void Deactivate()
-    {
+	public void Deactivate() {
 
-    }
+	}
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -13,14 +13,12 @@ public class PlayerMove : MonoBehaviour {
 	public float maxFallingSpeedForJumping = 0.5f;
 	public float jumpingWait = 2f;
 	//public float fallingBoost = 1;
-	public AudioClip shrink;
 	[Space]
 	public float gaseousFloatUpForce = 1;
 	public float gaseousShrinkDownForce = 1;
 	[Space]
 
 	[Header("Shrink")]
-
 	public float bonesForce;
 	public float bonesForceUp;
 	public float max_shrinking = 1.5f;
@@ -38,6 +36,9 @@ public class PlayerMove : MonoBehaviour {
 	public SphereCollider collider;
 	StateManager stateManager;
 	float shrinkage = 1;
+
+	[Space]
+	public AudioSource audioSource;
 
 	void Awake() {
 		rigidbody = GetComponent<Rigidbody>();
@@ -60,7 +61,6 @@ public class PlayerMove : MonoBehaviour {
 		if (shrinking) {
 			updateShrink();
 		}
-
 
 		if (stateManager.state == FlapperState.gaseous) {
 			rigidbody.isKinematic = true;
@@ -91,15 +91,10 @@ public class PlayerMove : MonoBehaviour {
 	void Shrink() {
 		shrinkage += Time.deltaTime * shrink_velocity;
 
-    }
-
-
-
+	}
 
 	IEnumerator JumpCoroutine() {
-
 		//Debug.Log(shrinking + " " + jumping);
-
 		if (shrinking && !jumping) {
 			jumping = true;
 			shrinking = false;
@@ -111,20 +106,16 @@ public class PlayerMove : MonoBehaviour {
 				}
 			}
 			shrinkage = 1;
+			audioSource.Play();
 			yield return new WaitForSeconds(jumpingWait);
 			jumping = false;
 		}
 	}
 
-
-
 	public void updateShrink() {
 		//AudioManager.singleton.PlayClip(shrink);
-
 		float force = bonesForce * shrinkage;
 		float forceUp = bonesForceUp * shrinkage;
-
-
 
 		//Down.MovePosition(Down.position + Vector3.down * shrinkage * -high * Time.deltaTime);
 		Up.MovePosition(Up.position + Vector3.up * forceUp * Time.deltaTime);
@@ -132,218 +123,5 @@ public class PlayerMove : MonoBehaviour {
 		Right.MovePosition(Right.position + Vector3.right * force * Time.deltaTime);
 		Front.MovePosition(Front.position + Vector3.forward * force * Time.deltaTime);
 		Back.MovePosition(Back.position + Vector3.back * force * Time.deltaTime);
-
-
-
 	}
-	/*
-     * public enum JumpType {
-	DilateShrink,
-	Shrink,
-	Dilate,
-	Scale
-    }
-    public Transform flapperModel;
-	public float speed = 5;
-	[Space]
-	public float jumpForce = 1;
-	public float doubleJumpMultiplier = 1.5f;
-	public float solidJumpForce = 1;
-	public float maxFallingSpeedForJumping = 0.5f;
-	public float jumpingWait = 1f;
-	//public float fallingBoost = 1;
-	public AudioClip shrink;
-	[Space]
-	public float gaseousFloatUpForce = 1;
-	public float gaseousShrinkDownForce = 1;
-	[Space]
-	public JumpType jumpType;
-	[Header("Dilate Shrink")]
-	public float toShrinkWait = 0.7f;
-	public float stopShrinkWait = 2f;
-	public float bonesForce;
-	public float bonesForceUp;
-	[Header("Shrink")]
-	public float toShrinkWait1 = 0.7f;
-	public float stopShrinkWait1 = 2f;
-	public float bonesForce1;
-	public float bonesForceUp1;
-	[Header("Dilate")]
-	public float toShrinkWait2 = 0.7f;
-	public float stopShrinkWait2 = 2f;
-	public float bonesForce2;
-	public float bonesForceUp2;
-	[Header("Dilate")]
-	public float toShrinkWait3 = 0.7f;
-	public float stopShrinkWait3 = 2f;
-	public float scaleXZ = 0.8f;
-	public float scaleY = 0.6f;
-	public float scaleSpeedXZ = 7;
-	public float scaleSpeedY = 7;
-	[Space]
-	public Rigidbody Up;
-	public Rigidbody Down, Left, Front, Right, Back;
-	public bool canMove, canJump = true;
-	[HideInInspector]
-	public bool jumping, shrinking = false;
-
-	[HideInInspector]
-	public Rigidbody rigidbody;
-	public SphereCollider collider;
-	StateManager stateManager;
-	int shrinkage = 0;
-	int shrinking_counter = 0;
-
-	void Awake() {
-		rigidbody = GetComponent<Rigidbody>();
-		collider = GetComponent<SphereCollider>();
-		stateManager = GetComponent<StateManager>();
-		jumping = false;
-	}
-
-	void FixedUpdate() {
-		if (canMove) {
-
-			if (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0) transform.position = Vector3.Lerp(transform.position, transform.position + (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward) / 1.3f, speed * Time.fixedDeltaTime);
-			else transform.position = Vector3.Lerp(transform.position, transform.position + (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward), speed * Time.fixedDeltaTime);
-
-			//rigidbody.MovePosition(rigidbody.position + (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward) * speed * Time.fixedDeltaTime);
-		}
-	}
-
-	void Update() {
-		if (shrinking_counter > 0) {
-			updateShrink();
-		} else {
-			flapperModel.localScale = new Vector3(Mathf.Lerp(flapperModel.localScale.x, 1, scaleSpeedXZ * Time.deltaTime), Mathf.Lerp(flapperModel.localScale.y, 1, scaleSpeedY * Time.deltaTime), Mathf.Lerp(flapperModel.localScale.z, 1, scaleSpeedXZ * Time.deltaTime));
-		}
-
-		if (stateManager.state == FlapperState.gaseous) {
-			rigidbody.isKinematic = true;
-			if (Input.GetButton("Jump")) {
-				transform.position = Vector3.Lerp(transform.position, transform.position - Vector3.up * 10, gaseousShrinkDownForce / 10 * Time.deltaTime);
-				//rigidbody.MovePosition(rigidbody.position + (Vector3.up * -gaseousShrinkDownForce * Time.deltaTime));
-			} else {
-				transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up, gaseousFloatUpForce * Time.deltaTime);
-				//rigidbody.MovePosition(rigidbody.position + (Vector3.up * gaseousFloatUpForce * Time.deltaTime));
-			}
-			rigidbody.isKinematic = false;
-		} else {
-			if (Input.GetButtonDown("Jump") && !shrinking && !jumping) {
-				if (stateManager.state == FlapperState.jelly && shrinkage < 2) {
-					Shrink();
-				} else if (stateManager.state == FlapperState.solid && shrinkage < 1) {
-					StartCoroutine(JumpCoroutine());
-				}
-			}
-		}
-
-		//if (!shrinking) shrinkage = 0;
-	}
-
-	void Shrink() {
-		shrinking = true;
-		shrinkage++;
-		shrinking_counter = 6;
-		StartCoroutine(ShrinkCoroutine());
-		StartCoroutine(JumpCoroutine());
-		
-	}
-	public void justShrink() {
-		shrinking = true;
-		shrinkage++;
-		shrinking_counter = 6;
-		StartCoroutine(ShrinkCoroutine());
-		jumping = false;
-	}
-
-	IEnumerator ShrinkCoroutine() {
-		float wait = toShrinkWait;
-		switch (jumpType) {
-			case JumpType.Shrink:
-				wait = toShrinkWait1;
-				break;
-			case JumpType.Dilate:
-				wait = toShrinkWait2;
-				break;
-			case JumpType.Scale:
-				wait = toShrinkWait3;
-				break;
-		}
-		yield return new WaitForSeconds(wait + shrinkage * 0.02f);
-		shrinking = false;
-		if (!jumping) {
-			yield return new WaitForSeconds(jumpingWait);
-			shrinkage = 0;
-		}
-
-
-	}
-
-	IEnumerator JumpCoroutine() {
-		float wait = stopShrinkWait;
-		switch (jumpType) {
-			case JumpType.Shrink:
-				wait = stopShrinkWait1;
-				break;
-			case JumpType.Dilate:
-				wait = stopShrinkWait2;
-				break;
-			case JumpType.Scale:
-				wait = stopShrinkWait3;
-				break;
-		}
-		yield return new WaitForSeconds(wait + shrinkage * 0.02f);
-		if (!shrinking && !jumping) {
-			jumping = true;
-			if (stateManager.state != FlapperState.gaseous && canJump) {
-				if (stateManager.state == FlapperState.solid) {
-					rigidbody.AddForce(Vector3.up * solidJumpForce, ForceMode.VelocityChange);
-				} else if (shrinkage == 1) {
-					rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
-				} else {
-					rigidbody.AddForce(Vector3.up * jumpForce * doubleJumpMultiplier, ForceMode.VelocityChange);
-				}
-			}
-			shrinkage = 0;
-			yield return new WaitForSeconds(jumpingWait);
-			jumping = false;
-		}
-	}
-
-	
-
-	public void updateShrink() {
-		//AudioManager.singleton.PlayClip(shrink);
-
-		float force = bonesForce * shrinkage;
-		float forceUp = bonesForceUp * shrinkage;
-		switch (jumpType) {
-			case JumpType.Shrink:
-				force = bonesForce1 * shrinkage;
-				forceUp = bonesForceUp1 * shrinkage;
-				break;
-			case JumpType.Dilate:
-				force = bonesForce2 * shrinkage;
-				forceUp = bonesForceUp2 * shrinkage;
-				break;
-			case JumpType.Scale:
-				force = 0;
-				forceUp = 0;
-				break;
-		}
-
-		//Down.MovePosition(Down.position + Vector3.down * shrinkage * -high * Time.deltaTime);
-		Up.MovePosition(Up.position + Vector3.up * forceUp * Time.deltaTime);
-		Left.MovePosition(Left.position + Vector3.left * force * Time.deltaTime);
-		Right.MovePosition(Right.position + Vector3.right * force * Time.deltaTime);
-		Front.MovePosition(Front.position + Vector3.forward * force * Time.deltaTime);
-		Back.MovePosition(Back.position + Vector3.back * force * Time.deltaTime);
-
-		if (jumpType == JumpType.Scale) {
-			flapperModel.localScale = new Vector3(Mathf.Lerp(flapperModel.localScale.x, scaleXZ, scaleSpeedXZ * Time.deltaTime), Mathf.Lerp(flapperModel.localScale.y, scaleY, scaleSpeedY * Time.deltaTime), Mathf.Lerp(flapperModel.localScale.z, scaleXZ, scaleSpeedXZ * Time.deltaTime));
-		}
-
-		shrinking_counter--;
-	}*/
 }
