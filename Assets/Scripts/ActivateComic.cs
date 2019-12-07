a5fbbfe420bad995cc19b1b54c75e09b5245b38b
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2019-12-07 13:18:01 +0100

    Comic updated (still missing: make first sentence disappear as the fourth appears etc.)

--- Assets/Scripts/ActivateComic.cs
+++ Assets/Scripts/ActivateComic.cs
@@ -6,15 +6,15 @@ using UnityEngine.UI;
 public class ActivateComic : MonoBehaviour, I_Activable
 {
     #region Attributes
-    //public bool comicOrientation;
+    public bool comicOrientation;
     public float duration;
-    public float textWaitTime;
     public float fadeTime;
     GameObject comicCloud;
     private Color color;
     private bool isActive = false;
     private bool activateAnimation = false;
-    private Vector3 standardScale;
+    private bool disactivateAnimation = false;
+    public Vector3 standardScale;
     private Vector3 littleScale;
     private Vector3 bigScale;
     private int animationPhase = 0;
@@ -24,19 +24,21 @@ public class ActivateComic : MonoBehaviour, I_Activable
     private string text;
     private Color textColor;
     public bool active = true;
+    private float stretchLerp = 0f;
     #endregion
 
     private void Start()
     {
         comicCloud = gameObject.transform.Find("ComicCloud").gameObject;
-        color = comicCloud.GetComponent<Image>().color;
+        comicCloud.transform.localScale = standardScale;
+        color = comicCloud.GetComponent<SpriteRenderer>().color;
         color.a = 0f;
         comicCloud.SetActive(false);
         textCloud = comicCloud.transform.GetChild(0).gameObject;
         textColor = textCloud.GetComponent<Text>().color;
-        standardScale = comicCloud.transform.localScale;
         bigScale = 1.1f * standardScale;
         littleScale = 0.9f * standardScale;
+        text = "";
         for (int k = 0; k < completeTextPieces.Length - 1; k++)
         {
             completeTextPieces[k] = completeTextPieces[k].ToUpper();
@@ -44,110 +46,115 @@ public class ActivateComic : MonoBehaviour, I_Activable
         }
         completeTextPieces[completeTextPieces.Length - 1] = completeTextPieces[completeTextPieces.Length - 1].ToUpper();
         completeText += completeTextPieces[completeTextPieces.Length - 1];
+        if (comicOrientation)
+            comicCloud.GetComponent<SpriteRenderer>().flipX = true;
     }
 
     private void Update()
     {
         if (activateAnimation)
         {
-            switch (animationPhase)
-            {
-                case 0:
-                default:
-                    StartCoroutine(Appear());
-                    break;
-                case 1:
-                    StartCoroutine(ChangeScale(standardScale, bigScale));
-                    break;
-                case 2:
-                    StartCoroutine(ChangeScale(bigScale, littleScale));
-                    break;
-                case 3:
-                    StartCoroutine(ChangeScale(littleScale, standardScale));
-                    break;
-                case 4:
-                    StartCoroutine(AnimateText(completeText));
-                    break;
-                case 5:
-                    StartCoroutine(WaitAmountOfTime());
-                    break;
-                case 6:
-                    StartCoroutine(Disappear());
-                    activateAnimation = false;
-                    break;
-            }
+            StartCoroutine(StartAnimation());
+            StartCoroutine(AnimateText());
+            activateAnimation = false;
+        }
+        else if (disactivateAnimation)
+        {
+            StopAllCoroutines();
+            StartCoroutine(EndAnimation());
+            disactivateAnimation = false;
         }
     }
 
-    IEnumerator Appear()
-    {
-        for (float a = 0; color.a < 1; a += Time.deltaTime / fadeTime)
-        {
-            color.a = Mathf.Clamp(a, 0, 1);
-            comicCloud.GetComponent<Image>().color = color;
-            yield return null;
-        }
-        animationPhase++;
-        StopAllCoroutines();
+    IEnumerator StartAnimation()
+    {        
+        Appear();
+        Stretch();
+        yield return new WaitForSeconds(Time.deltaTime / fadeTime);
+        yield return StartAnimation();
     }
 
-    IEnumerator ChangeScale(Vector3 start, Vector3 finish)
+    private void Appear()
     {
-        for (float j = 0; j < 1; j += Time.deltaTime)
-        {
-            comicCloud.transform.localScale = Vector3.Lerp(start, finish, j);
-            yield return null;
-        }
-        animationPhase++;
-        StopAllCoroutines();
+        color.a += Time.deltaTime / fadeTime;
+        color.a = Mathf.Clamp(color.a, 0, 1);
+        textColor.a = color.a;
+        comicCloud.GetComponent<SpriteRenderer>().color = color;
+        textCloud.GetComponent<Text>().color = textColor;
     }
 
-    IEnumerator AnimateText(string completeText)
+    private void Stretch()
     {
-        int i = 0;
-        text = "";
-        while (i < completeText.Length)
+        switch (animationPhase)
         {
-            text += completeText[i++];
+            case 0:
+                comicCloud.transform.localScale = Vector3.Lerp(standardScale, bigScale, stretchLerp);
+                break;
+            case 1:
+                comicCloud.transform.localScale = Vector3.Lerp(bigScale, littleScale, stretchLerp);
+                break;
+            case 2:
+                comicCloud.transform.localScale = Vector3.Lerp(littleScale, standardScale, stretchLerp);
+                break;
+            default:
+                break;
+        }
+        stretchLerp += (Time.deltaTime / fadeTime) * 3;
+        if (stretchLerp > 1)
+        {
+            animationPhase++;
+            stretchLerp = 0f;
+        }
+    }
+
+
+    IEnumerator AnimateText()
+    {
+        int index;
+        for (index = 0 ; index < completeText.Length; index++)
+        {
+            text += completeText[index];
             textCloud.GetComponent<Text>().text = text;
-            yield return new WaitForSeconds(textWaitTime);
+            yield return new WaitForSeconds(fadeTime / completeText.Length);
         }
-        animationPhase++;
-        StopAllCoroutines();
-    }
-
-    IEnumerator WaitAmountOfTime()
-    {
-        yield return new WaitForSeconds(textWaitTime);
-        animationPhase++;
-        StopAllCoroutines();
-    }
-
-    IEnumerator Disappear()
-    {
-        for (float a = 1; color.a > 0; a -= Time.deltaTime / fadeTime)
+        if (index == completeText.Length)
         {
-            color.a = Mathf.Clamp(a, 0, 1);
-            textColor.a = color.a;
-            comicCloud.GetComponent<Image>().color = color;
-            textCloud.GetComponent<Text>().color = textColor;
-            yield return null;
+            yield return new WaitForSeconds(duration);
+            disactivateAnimation = true;
         }
-        animationPhase = 0;
-        activateAnimation = false;
-        StopAllCoroutines();
     }
 
+    IEnumerator EndAnimation()
+    {
+        Disappear();
+        yield return new WaitForSeconds(Time.deltaTime / fadeTime);
+        yield return EndAnimation();
+    }
 
+    private void Disappear()
+    {
+        color.a -= Time.deltaTime / fadeTime;
+        color.a = Mathf.Clamp(color.a, 0, 1);
+        textColor.a = color.a;
+        comicCloud.GetComponent<SpriteRenderer>().color = color;
+        textCloud.GetComponent<Text>().color = textColor;
+        if(color.a <= 0)
+        {
+            activateAnimation = false;
+            disactivateAnimation = false;
+            StopAllCoroutines();
+        }
+    }
 
     public void Activate(bool type = true)
     {
         if (!isActive)
         {
             comicCloud.SetActive(true);
-            comicCloud.GetComponent<Image>().color = color;
+            comicCloud.GetComponent<SpriteRenderer>().color = color;
             isActive = true;
             activateAnimation = true;
+            animationPhase = 0;
         }
     }
 
