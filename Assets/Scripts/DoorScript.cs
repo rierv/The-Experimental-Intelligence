b9c0d64f54583a695d5352af30f81f2c61c4ef85
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-12-11 18:18:40 +0100

    Partial semi-revert.

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -7,9 +7,11 @@ public class DoorScript : MonoBehaviour, I_Activable {
 	public float upperLockerY;
 	public float lowerLockerY;
 	public float doorOpenTime;
-	bool goingUp = false;
+	public bool goingUp = true;
+
+	bool isActive = false;
 	bool stopped = false;
-	private Vector3 translateVector;
+	Vector3 translateVector;
 	bool activable = true;
 	#endregion
 
@@ -18,32 +20,38 @@ public class DoorScript : MonoBehaviour, I_Activable {
 	}
 
 	private void FixedUpdate() {
-		if (goingUp) {
-			if ((upperLockerY - transform.position.y) > 0) {
+		if (isActive) {
+			if (goingUp && (upperLockerY - transform.position.y) > 0) {
 				transform.position = Vector3.Lerp(transform.position, transform.position + translateVector, Time.deltaTime * doorOpenTime);
+			} else if (!goingUp && (transform.position.y - upperLockerY) > 0) {
+				transform.position = Vector3.Lerp(transform.position, transform.position - translateVector, Time.deltaTime * doorOpenTime);
 			}
-		} else if (!stopped && (transform.position.y - lowerLockerY) > 0) {
+		} else if (goingUp && (transform.position.y - lowerLockerY) > 0) {
 			transform.position = Vector3.Lerp(transform.position, transform.position - translateVector, Time.deltaTime * doorOpenTime);
+		} else if (!goingUp && (lowerLockerY - transform.position.y) > 0) {
+			transform.position = Vector3.Lerp(transform.position, transform.position + translateVector, Time.deltaTime * doorOpenTime);
 		}
 	}
 
 	private void OnCollisionStay(Collision collision) {
-		if (!goingUp && (collision.gameObject.GetComponent<StateManager>() && collision.gameObject.GetComponent<StateManager>().state == FlapperState.solid || collision.gameObject.layer == 13)) {
+		if (collision.gameObject.GetComponent<StateManager>() && collision.gameObject.GetComponent<StateManager>().state == FlapperState.solid) {
+			isActive = false;
+		} else if (!isActive && collision.gameObject.layer == 13) {
 			stopped = true;
 		}
 	}
 	private void OnCollisionExit(Collision collision) {
-		if (stopped && (collision.gameObject.GetComponent<StateManager>() && collision.gameObject.GetComponent<StateManager>().state == FlapperState.solid || collision.gameObject.layer == 13)) {
+		if (stopped && collision.gameObject.layer == 13) {
 			stopped = false;
 		}
 	}
 
 	public void Activate(bool type = true) {
-		if (activable) goingUp = true;
+		if (activable) isActive = true;
 	}
 
 	public void Deactivate() {
-		goingUp = false;
+		isActive = false;
 	}
 
 	public void canActivate(bool enabled) {
