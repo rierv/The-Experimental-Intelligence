cc09cbe61cb937218f58f9b4027dd9f1a0b46a02
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-12-16 16:44:52 +0100

    Disabled pushable script. Updated Push stuff. Moved EventSystem in Game Manager prefab.

---
+++ Assets/Scripts/PushableHeavy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableHeavy : MonoBehaviour {
+	Rigidbody rigidbody;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void OnTriggerStay(Collider other) {
+		StateManager flapper = other.GetComponent<StateManager>();
+		if (flapper) {
+			if (flapper.state == FlapperState.solid) {
+				rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+			} else {
+				rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+			}
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.GetComponent<StateManager>()) {
+			rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+		}
+	}
+}
