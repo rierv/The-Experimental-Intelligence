b31383dac7f311f5823b603e4c66523707d1a73a
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-12-29 12:08:03 +0100

    Fixed coal machine. Added CameraNewOffset to rotate camera.

---
+++ Assets/Scripts/Camera/CameraNewOffset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraNewOffset : MonoBehaviour {
+	public Vector3 offset;
+	public bool overwriteX;
+	public bool overwriteY;
+	public bool overwriteZ;
+
+	CameraController cameraController;
+	Vector3 previousOffset;
+
+	void Awake() {
+		cameraController = FindObjectOfType<CameraController>();
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<JellyCore>()) {
+			previousOffset = new Vector3(cameraController.xOffset, cameraController.yOffset, cameraController.zOffset);
+			if (overwriteX)
+				cameraController.xOffset = offset.x;
+			if (overwriteY)
+				cameraController.yOffset = offset.y;
+			if (overwriteZ)
+				cameraController.zOffset = offset.z;
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.GetComponent<JellyCore>()) {
+			if (overwriteX)
+				cameraController.xOffset = previousOffset.x;
+			if (overwriteY)
+				cameraController.yOffset = previousOffset.y;
+			if (overwriteZ)
+				cameraController.zOffset = previousOffset.z;
+		}
+	}
+}
