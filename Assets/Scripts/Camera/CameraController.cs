50a592cc72aa8c696e6de165dde7e6dd56ad7a11
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-12-29 12:15:20 +0100

    Smoothed camera movement and rotation.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -2,47 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    public float cameraSpeed = 1;
-    public Transform pointer;
-    private Transform target;
-    public Transform secondTarget;
+public class CameraController : MonoBehaviour {
+	public float cameraSpeed = 0.3f;
+	public float maxSpeed = 50;
+	public float cameraRotationSpeed = 6;
+	[Space]
+	public Transform pointer;
+	private Transform target;
+	public Transform secondTarget;
 
-    public bool allowLookAtTarget=false;
-    [Header("To lock an axis, freeze set 0 to min and max")]
-    public float minX = -100;
-    public float maxX = 100;
-    public float xOffset = 0;
-    [Space]
-    public float minY = -100;
-    public float maxY = 100;
-    public float yOffset = 5;
+	public bool allowLookAtTarget = false;
+	[Header("To lock an axis, set min and max to 0")]
+	public float minX = -100;
+	public float maxX = 100;
+	public float xOffset = 0;
+	[Space]
+	public float minY = -100;
+	public float maxY = 100;
+	public float yOffset = 5;
 
-    [Space]
-    public float minZ = 0;
-    public float maxZ = 0;
-    public float zOffset = -10;
+	[Space]
+	public float minZ = 0;
+	public float maxZ = 0;
+	public float zOffset = -10;
 
-    public Vector3 secondTargetOffset = Vector3.zero;
-    private Vector3 directionOnSecondTarget = Vector3.zero;
-	
+	public Vector3 secondTargetOffset = Vector3.zero;
+	private Vector3 directionOnSecondTarget = Vector3.zero;
+
 	[Space]
 	public float initialDelay = 3;
 
 	bool lookAtFlapper;
 	Vector3 initPosition;
 	Quaternion initRotation;
+	//Vector3 initOffset;
+	Vector3 velocity = Vector3.zero;
 
-    void Awake()
-    {
+	void Awake() {
 		initPosition = transform.position;
 		initRotation = transform.rotation;
+		//initOffset = new Vector3(xOffset, yOffset, zOffset);
 
-        if(target==null) target = FindObjectOfType<JellyCore>().transform;
-        //transform.position=Vector3.Lerp(transform.position, new Vector3(Mathf.Clamp(target.transform.position.x, minX, maxX)+ xOffset, Mathf.Clamp(target.transform.position.y, minY, maxY) + yOffset, Mathf.Clamp(target.transform.position.z, minZ, maxZ) + zOffset), Time.deltaTime*cameraSpeed);
-    }
-	
+		if (target == null) target = FindObjectOfType<JellyCore>().transform;
+		//transform.position=Vector3.Lerp(transform.position, new Vector3(Mathf.Clamp(target.transform.position.x, minX, maxX)+ xOffset, Mathf.Clamp(target.transform.position.y, minY, maxY) + yOffset, Mathf.Clamp(target.transform.position.z, minZ, maxZ) + zOffset), Time.deltaTime*cameraSpeed);
+	}
+
 	void Start() {
 		StartCoroutine(InitialCoroutine());
 	}
@@ -51,23 +55,31 @@ public class CameraController : MonoBehaviour
 		lookAtFlapper = true;
 	}
 
-    void Update()
-    {
+	void Update() {
 		if (Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown("joystick button 2")) { // xbox button X
 			lookAtFlapper = !lookAtFlapper;
 		}
 		if (lookAtFlapper) {
 			Vector3 newPosition = new Vector3(Mathf.Clamp(target.transform.position.x, minX, maxX) + xOffset, Mathf.Clamp(target.transform.position.y, minY, maxY) + yOffset, Mathf.Clamp(target.transform.position.z, minZ, maxZ) + zOffset);
-			transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * cameraSpeed);
-			if (allowLookAtTarget)
-			{
-				if (secondTarget != null) directionOnSecondTarget =  secondTarget.position - target.transform.position;
+			//transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * cameraSpeed);
+			transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, cameraSpeed, maxSpeed);
+			if (allowLookAtTarget) {
+				if (secondTarget != null) directionOnSecondTarget = secondTarget.position - target.transform.position;
 				pointer.transform.LookAt(target.transform.position + directionOnSecondTarget + secondTargetOffset);
-				transform.rotation = Quaternion.Lerp(transform.rotation, pointer.rotation, cameraSpeed * Time.deltaTime);
+				//transform.rotation = Quaternion.Lerp(transform.rotation, pointer.rotation, cameraRotationSpeed * Time.deltaTime);
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, pointer.rotation, cameraRotationSpeed * Time.deltaTime * Quaternion.Angle(transform.rotation, pointer.rotation));
 			}
 		} else {
-			transform.position = Vector3.Lerp(transform.position, initPosition, cameraSpeed * Time.deltaTime);
-			transform.rotation = Quaternion.Lerp(transform.rotation, initRotation, cameraSpeed * Time.deltaTime);
+			//transform.position = Vector3.Lerp(transform.position, initPosition, cameraSpeed * Time.deltaTime);
+			transform.position = Vector3.SmoothDamp(transform.position, initPosition, ref velocity, cameraSpeed, maxSpeed);
+			//transform.rotation = Quaternion.Lerp(transform.rotation, initRotation, cameraRotationSpeed * Time.deltaTime);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, initRotation, cameraRotationSpeed * Time.deltaTime * Quaternion.Angle(transform.rotation, initRotation));
 		}
-    }
+	}
+
+	/*public void ResetOffset() {
+		xOffset = initOffset.x;
+		yOffset = initOffset.y;
+		zOffset = initOffset.z;
+	}*/
 }
