c180208c2b56071b937af1611963f744d5f5e9c4
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2020-01-05 09:54:39 +0100

    Improved move between levels using keypad + and -.

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Screenshot : MonoBehaviour {
-#if UNITY_EDITOR
 	void Update() {
+#if UNITY_EDITOR
 		if (Input.GetKeyDown(KeyCode.Keypad0)) {
 			Time.timeScale = 0;
 			transform.GetChild(0).gameObject.SetActive(false);
 			ScreenCapture.CaptureScreenshot("Assets\\Resources\\Levels\\" + SceneManager.GetActiveScene().name + ".png");
 			Debug.Log("Screenshot Assets\\Resources\\Levels\\" + SceneManager.GetActiveScene().name + ".png");
-		} else if (Input.GetKeyDown(KeyCode.KeypadPlus)) {
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+#endif
+		if (Input.GetKeyDown(KeyCode.KeypadPlus)) {
+			int i = SceneManager.GetActiveScene().buildIndex;
+			if (i == SceneManager.sceneCountInBuildSettings - 1) {
+				i = 0;
+			} else {
+				i++;
+			}
+			SceneManager.LoadScene(i);
 		} else if (Input.GetKeyDown(KeyCode.KeypadMinus)) {
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+			int i = SceneManager.GetActiveScene().buildIndex;
+			if (i > 0) {
+				i--;
+				SceneManager.LoadScene(i);
+			}
 		}
 	}
-#endif
 }
