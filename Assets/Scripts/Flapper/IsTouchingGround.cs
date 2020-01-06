a38b10a4ead0b11fc918b7163194c7e442a3a689
jebbo <r.iervolino93@gmail.com>
2020-01-06 20:12:04 +0100

    stuff

--- Assets/Scripts/Flapper/IsTouchingGround.cs
+++ Assets/Scripts/Flapper/IsTouchingGround.cs
@@ -6,8 +6,9 @@ public class IsTouchingGround : MonoBehaviour {
 	PlayerMove playerMove;
 	int colliderCount = 0;
 	Vector3 offset;
+    bool waitForLanding = false;
 
-	void Awake() {
+    void Awake() {
 		playerMove = GetComponentInParent<PlayerMove>();
 		offset = transform.localPosition;
 	}
@@ -31,12 +32,19 @@ public class IsTouchingGround : MonoBehaviour {
 	}
 
 	void ApplyChanges() {
-		if (colliderCount > 0) {
+		if (colliderCount > 0 && !playerMove.canJump && !waitForLanding) {
 			playerMove.canJump = true;
 			playerMove.collider.material.frictionCombine = PhysicMaterialCombine.Average;
-		} else {
-			playerMove.canJump = false;
+		} else if (colliderCount <= 0 && playerMove.canJump && waitForLanding) {
 			playerMove.collider.material.frictionCombine = PhysicMaterialCombine.Minimum;
+            StartCoroutine(StopJumping());
 		}
 	}
+    IEnumerator StopJumping()
+    {
+        waitForLanding = true;
+        yield return new WaitForSeconds(.05f);
+        waitForLanding = false;
+        playerMove.canJump = false;
+    }
 }
