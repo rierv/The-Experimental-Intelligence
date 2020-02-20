976a8e9404826a23aa503b03185651746d17a818
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2020-02-20 15:29:52 +0100

    Added Credits panel.

---
+++ Assets/Scripts/ShowHideCredits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideCredits : MonoBehaviour
+{
+	public GameObject credits;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<JellyCore>()) {
+			credits.SetActive(true);
+		}
+	}
+	private void OnTriggerExit(Collider other) {
+		if (other.GetComponent<JellyCore>()) {
+			credits.SetActive(false);
+		}
+	}
+}
